f27540030bc90f7499995158feb7e6c7d0a967ca
Bill Birch <Bill.Birch@coles.com.au>
2020-05-11 20:40:30 +1000

    Add max depth , and concat when one child and split on non-alphanum.

--- frangipanni.go
+++ frangipanni.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"unicode"
 )
 
 type node struct {
@@ -15,50 +16,67 @@ type node struct {
 	children []*node
 }
 
-func ptree(t *node, d int) {
+func ptree(t *node, max int, d int) {
+	if d > max {
+		return
+	}
 	for i := 0; i < d; i++ {
 		fmt.Printf("  ")
 	}
+	for len(t.children) == 1 {
+		fmt.Printf("%s ", t.prefix)
+		t = t.children[0]
+	}
 	fmt.Printf("%s:\n", t.prefix)
 	for _, c := range t.children {
-		ptree(c, d+1)
+		ptree(c, max, d+1)
 	}
 }
-func add(tree *node, tok []string, depth int) {
+func add(tree *node, tok []string, max int, depth int) {
 	//fmt.Printf("add %d node %s %s\n", depth, tree.prefix, tok)
 	if len(tok) < 1 {
 		return
 	}
+	if depth == max {
+		x := node{strings.Join(tok, " "), []*node{}}
+		tree.children = append(tree.children, &x)
+		return	
+	}
 	for _, c := range tree.children {
 		//fmt.Printf("children %d node %s child %d %s\n", depth, tree.prefix, i, c.prefix)
 		if tok[0] == c.prefix {
-			add(c, tok[1:], depth+1)
+			add(c, tok[1:], max, depth+1)
 			return
 		}
 	}
 	// So not a match to the children. It's a new child.
 	x := node{tok[0], []*node{}}
 	tree.children = append(tree.children, &x)
-	add(&x, tok[1:], depth+1)
+	add(&x, tok[1:], max, depth+1)
 	//fmt.Printf("newchild %d %s\n", depth, tree)
 }
 
 func main() {
+	max := 100
 	file := os.Stdin
 	defer file.Close()
-	fs := "/"
+//	fs := " "
+	f := func(c rune) bool {
+		return /* c != '-' && */ !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
 
 	root := node{".", nil}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		l := scanner.Text()
-		t := strings.Split(l, fs)
+		t := strings.FieldsFunc(l, f)
+		// t := strings.Split(l, fs)
 		//fmt.Printf("read %s\n", t)
-		add(&root, t, 0)
+		add(&root, t, max, 0)
 		//ptree(&root, 0)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	ptree(&root, -1)
+	ptree(&root, max+1, -1)
 }
