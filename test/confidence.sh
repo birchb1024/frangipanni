b1eb2b9b9c131f168961b7dc30ef343beaa7e773
Peter William Birch <birchb1024@gmail.com>
2020-05-17 20:41:31 +1000

    Remove duplicate switch on sort order

--- test/confidence.sh
+++ test/confidence.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -euo pipefail
-scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scriptdir="$(readlink -f $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
 
 tempfile=$(mktemp)
-for tf in $scriptdir/fixtures/*
+cd $scriptdir   # So all paths arelative to here in test data file
+for tf in fixtures/*
 do
     for sw in '' -chars '-breaks /' -separators -counts '-level 2' '-depth 2' '-format json'
     do
         echo "$tf---$sw----------------------------------------------------------------------------------------------------" >> "$tempfile"
-        $scriptdir/../frangipanni $sw <"$tf" >> "$tempfile"
+        ../frangipanni $sw <"$tf" >> "$tempfile"
     done
 done
 set -x
