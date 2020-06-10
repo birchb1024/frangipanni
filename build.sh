8768b88006b26bd988e2dbb428a750053dbd1754
Bill Birch <Bill.Birch@coles.com.au>
2020-06-10 22:50:54 +1000

    Add Lua files to package

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@ go build
 test/confidence.sh
 
 GOOS=windows GOARCH=386 go build -o frangipanni.exe frangipanni.go
+GOOS=darwin GOARCH=amd64 go build -o frangipanni_mac frangipanni.go
 
 rm -f frangipanni.zip rm -f frangipanni.tgz
-tar zcvf frangipanni.tgz frangipanni
-zip frangipanni.zip frangipanni.exe
+tar zcvf frangipanni.tgz frangipanni *.lua
+zip frangipanni.zip frangipanni.exe frangipanni_mac *.lua
 
 ls -ltr
